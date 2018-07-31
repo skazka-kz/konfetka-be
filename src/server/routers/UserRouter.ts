dd1d76ea58cee12e91d5caa433910a5f7eba343c
Slava Bezgachev <bezgachev@gmail.com>
2018-07-31 17:58:02 +0600

    Created the create user function, no tests yet

--- src/server/routers/UserRouter.ts
+++ src/server/routers/UserRouter.ts
@@ -1,7 +1,49 @@
 import { Request, Response, Router } from "express";
+import { IUserDocument } from "../interfaces/UserDocument";
 import User from "../models/User";
 
+interface IUserProps {
+  email: string;
+  fullName?: string;
+  nickName?: string;
+  password: string;
+}
+
 class UserRouter {
+  /**
+   * Validates and creates a new user, saves to Mongoose
+   * @param up IUserProps: object with the values for a new user
+   */
+  public static registerUser(up: IUserProps): Promise<IUserDocument> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        if (!up.email) {
+          reject("Error: No email provided");
+        }
+        if (!up.password) {
+          reject("Error: No password provided");
+        }
+        if (!UserRouter.validateEmail(up.email)) {
+          reject("Error: Invalid email format");
+        }
+
+        const newUser = new User(up);
+        newUser.save();
+        // Hide password from the response
+        newUser.password = undefined;
+        resolve(newUser);
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }
+
+  public static validateEmail(email) {
+    // tslint:disable-next-line:max-line-length
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regEx.test(String(email).toLowerCase());
+  }
+
   public router: Router;
 
   constructor() {
@@ -14,11 +56,27 @@ class UserRouter {
     return res.send(users);
   }
   public async GetUser(req: Request, res: Response) {
-    const { id }: any = req.param;
+    const { id }: any = req.params;
     const user = await User.findById(id);
     return res.send(user);
   }
-  public CreateUser(req: Request, res: Response) {}
+  public async CreateUser(req: Request, res: Response) {
+    const { email, password, fullName, nickName }: any = req.params;
+
+    const user = new User({
+      email,
+      password,
+      fullName,
+      nickName
+    });
+
+    try {
+      const createdUser = await UserRouter.registerUser(user);
+      return res.send(createdUser);
+    } catch (e) {
+      return res.status(400).send({ message: e.message });
+    }
+  }
   public UpdateUser(req: Request, res: Response) {}
   public DeleteUser(req: Request, res: Response) {}
 
