11a8a0138aff3ff2e712d1426a5155dd65ae45ba
Slava Bezgachev <bezgachev@gmail.com>
2018-08-11 18:57:54 +0600

    Fully finished the login test, all with smarter cookie checking

--- src/server/__tests__/models/auth.ts
+++ src/server/__tests__/models/auth.ts
@@ -36,5 +36,22 @@ describe("User tests, both Mongoose model and REST API", () => {
 
     const response = await request.post("/api/v1/auth/login").send(loginProps);
     expect(response.status).toBe(200);
+    // Should return cookies, named session and session.sig, with reasonable expiration date
+    expect(response.header["set-cookie"]).toBeDefined();
+    expect(response.header["set-cookie"].length).toBe(2);
+    expect(response.header["set-cookie"][0]).toMatch(/session=/);
+    expect(response.header["set-cookie"][1]).toMatch(/session.sig=/);
+    const sessionCookie = response.header["set-cookie"][0];
+    // The cookie looks like this: session=blabla; path=/; expires=Mon, 10 Sep 2018 12:35:49 GMT; httponly
+    // Chop it up to just get the date string
+    const expirationDate = new Date(
+      sessionCookie.slice(
+        sessionCookie.indexOf("expires=") + 8,
+        sessionCookie.indexOf("; httponly")
+      )
+    );
+    const aMonthInMilliseconds = 31 * 24 * 60 * 60 * 1000;
+    // Cookie expiration should be 30 days,
+    expect(Date.now() - expirationDate.getTime()).toBeLessThan(aMonthInMilliseconds);
   });
 });
