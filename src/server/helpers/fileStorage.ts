c107cbb8d6f77076d9f52c829dbbb736c5ab6c33
Slava Bezgachev <bezgachev@gmail.com>
2018-08-17 19:08:41 +0600

    copied multer config, need to re-check

--- src/server/helpers/fileStorage.ts
+++ src/server/helpers/fileStorage.ts
@@ -1,6 +1,32 @@
 import * as multer from "multer";
+import * as path from "path";
+import * as uniqid from "uniqid";
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {},
-  filename: (req, file, cb) => {}
+  destination: (req, file, cb) => {
+    if (process.env.NODE_ENV === "test") {
+      cb(null, "./uploads/test");
+    } else {
+      cb(null, "./uploads");
+    }
+  },
+  filename: (req, file, cb) => {
+    let extension = path.extname(file.originalname);
+    if (!extension) {
+      switch (file.mimetype) {
+        case "image/jpeg":
+          extension = ".jpg";
+          break;
+        case "image/png":
+          extension = ".png";
+          break;
+        case "image/gif":
+          extension = ".gif";
+          break;
+        default:
+          extension = ".jpg";
+      }
+    }
+    cb(null, `product_image-${uniqid()}${extension}`);
+  }
 });
