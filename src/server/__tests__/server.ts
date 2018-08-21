37fc8bd7df9d05ae23230def5d7727b0ea724dbd
Slava Bezgachev <bezgachev@gmail.com>
2018-08-22 00:44:39 +0600

    minor cleanup

--- src/server/__tests__/server.ts
+++ src/server/__tests__/server.ts
@@ -3,28 +3,14 @@ import "jest";
 import supertest = require("supertest");
 import Server from "../server";
 
-let app: Application;
-let request: supertest.SuperTest<supertest.Test>;
+const app: Application = new Server().app;
+const request: supertest.SuperTest<supertest.Test> = supertest(app);
 
 describe("Server App tests", () => {
-  beforeEach(() => {
-    app = new Server().app;
-    request = supertest(app);
-  });
-
-  afterEach(() => {
-    app = undefined;
-    request = undefined;
-  });
-
   test("/ping returns the right code and content", async () => {
     const response: any = await request.get("/ping");
-    const value = {
-      some: "Value",
-      other: 255
-    };
+
     expect(response.statusCode).toBe(200);
     expect(response.body.message).toBe("Pong");
   });
-
 });
